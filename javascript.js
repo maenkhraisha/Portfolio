517474550fe9274cb85c6e1a6e2aff5cb869c19c
maen <maen.1980@yahoo.com>
2022-03-01 16:49:12 +0300

    change the first review request

--- javascript.js
+++ javascript.js
@@ -1,11 +1,27 @@
 const menuBtn = document.getElementById('menu-btn');
 const menuList = document.getElementById('menu-list');
-const menuclose = document.getElementById('menu-close-btn');
+const menuClose = document.getElementById('menu-close-btn');
+
+const menuPortfolio = document.getElementById('btn-porfolio-menu');
+const menuAbout = document.getElementById('btn-about-menu');
+const menuContact = document.getElementById('btn-contact-menu');
 
 menuBtn.addEventListener('click', () => {
   menuList.style.display = 'flex';
 });
 
-menuclose.addEventListener('click', () => {
+menuClose.addEventListener('click', () => {
+  menuList.style.display = 'none';
+});
+
+menuPortfolio.addEventListener('click',()=>{
+  menuList.style.display = 'none';
+});
+
+menuAbout.addEventListener('click',()=>{
+  menuList.style.display = 'none';
+});
+
+menuContact.addEventListener('click',()=>{
   menuList.style.display = 'none';
 });
