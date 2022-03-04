77f3f62043d9476418057b7aaf6fe9309d33b42b
kosher <lionel.kossi@gmail.com>
2022-03-04 17:49:19 +0000

    Add local storage for user data

--- local-storage.js
+++ local-storage.js
@@ -1,25 +1,95 @@
-let userFName = document.getElementById('first-name');
-const userLName = document.getElementById('last-name');
-const userEmail = document.getElementById('email');
-const message = document.getElementById('text-area');
+let userFNameField = document.querySelector('#first-name input');
+let userLNameField = document.querySelector('#last-name input');
+let userEmailField = document.getElementById('email');
+let messageField = document.getElementById('text-area');
 
-function setStorage() {
+function createLocalStorage(){
+  localStorage.setItem('fname', "");
+  localStorage.setItem('lname', "");
+  localStorage.setItem('email', "");
+  localStorage.setItem('message', "");
+}
+
   const userInfo = {
-    FName: userFName,
-    LName: userLName,
-    email: userEmail,
-    message,
-  };
-  localStorage.setItem('info', userInfo);
-}
-function setStyles() {
-  userFName = localStorage.getItem('info');
+    FName: localStorage.getItem('fname'),
+    LName: localStorage.getItem('lname'),
+    email: localStorage.getItem('email'),
+    message: localStorage.getItem('message'),
+  }
+
+
+function loadLocalStorage(){
+  userFNameField.value = localStorage.getItem('fname')
+  userLNameField.value = localStorage.getItem('lname')
+  userEmailField.value = localStorage.getItem('email')
+  messageField.value = localStorage.getItem('message')
 }
 
-window.addEventListener('storage', () => {
-  if (!localStorage.getItem('info')) {
-    setStorage();
-  } else {
-    setStyles();
-  }
+function updateLocalStorage(){
+  localStorage.setItem('fname', userInfo.FName)
+  localStorage.setItem('lname', userInfo.LName)
+  localStorage.setItem('email', userInfo.email)
+  localStorage.setItem('message', userInfo.message)
+}
+
+createLocalStorage()
+  
+// function setStyles() {
+//   userFNAme = localStorage.getItem('info');
+// }
+
+function addEvent(element, event, callback) {
+  const previousEventCallBack = element[`on${event}`];
+  element[`on${event}`] = function (e) {
+    let output = callback(e);
+
+    if (output === false) return false;
+
+    if (typeof previousEventCallBack === 'function') {
+      output = previousEventCallBack(e);
+      if (output === false) return false;
+    }
+    return false;
+  };
+}
+
+addEvent(userFNameField, 'input', () => {
+
+  userInfo.FName = userFNameField.value;
+  updateLocalStorage()
+  console.log(userInfo.FName)
+
+});
+
+addEvent(userLNameField, 'input', () => {
+
+  userInfo.LName = userLNameField.value
+  updateLocalStorage()
+  console.log(userInfo.LName)
+});
+
+addEvent(userEmailField, 'input', () => {
+
+  userInfo.email = userEmailField.value
+  // localStorage.setItem('lname', userInfo.email)
+  updateLocalStorage()
+  console.log(userInfo.email)
+});
+
+addEvent(messageField, 'input', () => {
+
+  userInfo.message = messageField.value
+  updateLocalStorage()
+  console.log(messageField.value)
+});
+
+
+
+window.addEventListener('load', () => {
+
+  updateLocalStorage()
+
+  loadLocalStorage()
+
+  console.log("life")
 });
