ce2fa016d70fedd0164d7fecd3c528071ecbc638
maen <maen.1980@yahoo.com>
2022-03-05 00:08:18 +0300

    validate with lowercase and dot and @

--- validation.js
+++ validation.js
@@ -1,24 +1,39 @@
-console.log(email); 
-console.log(errorSpan); 
-console.log(form); 
-email.addEventListener('input', function(event) {
-     if(email.validity.valid){
-          errorSpan.textContent = ''; 
-        }else { 
-            errorSpan.textContent = 'Email address must be lowercase'; 
-            errorSpan.style.display = 'block'; 
-        } 
-    }); 
+// let errorSpan = document.getElementById('error');
+// let email = document.getElementById('email');
+// let form = document.getElementsByTagName('form');
 
-    /* if the validation is invalid stop form from send */ 
-    form.addEventListener('submit', function(event){
-         if(!email.validity.valid){ 
-             showError();
-              event.preventDefault();
-             }
-        }); 
-        
-    function showError(){
-        errorSpan.textContent = 'Email address must be lowercase';
-        errorSpan.style.display = 'block'; 
-    } 
+
+// function validateEmail(){
+//     const lowerCase = /^[a-z0-9.@]+$/;
+//     let result = lowerCase.test(email.value);
+//     return result;
+// }
+
+// function showError(){
+//     errorSpan.style.display = 'block';
+//     errorSpan.textContent = 'Email address must be lowercase'; 
+// }
+// function hideError(){
+//     errorSpan.style.display = 'none';
+//     errorSpan.textContent = ''; 
+// }
+
+// email.addEventListener('input', () =>{
+//     if(!validateEmail()){
+//         showError();
+//     }
+//     else{
+//         hideError();
+//     }
+// }); 
+
+//     /* if the validation is invalid stop form from send */ 
+// form.addEventListener('submit', function(event){
+//     if(!validateEmail()){ 
+//         showError();
+//         event.preventDefault();
+//     }
+//     else{
+//         form.submit();
+//     }
+// });
