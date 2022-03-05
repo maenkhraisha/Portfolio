a3c7e35bf3e6b56af42474264cd3c3cf837c65dc
maen <maen.1980@yahoo.com>
2022-03-05 13:52:38 +0300

    fix linter

--- validation.js
+++ validation.js
@@ -1,36 +1,34 @@
-let errorSpan = document.getElementById('error');
-let email = document.getElementById('email');
-let form = document.getElementById('contact-form');
+const errorSpan = document.getElementById('error');
+const email = document.getElementById('email');
+const form = document.getElementById('contact-form');
 
-
-function validateEmail(){
-    const lowerCase = /^[a-z0-9.@]+$/;
-    let result = lowerCase.test(email.value);
-    return result;
+function validateEmail() {
+  const lowerCase = /^[a-z0-9.@]+$/;
+  const result = lowerCase.test(email.value);
+  return result;
 }
 
-function showError(){
-    errorSpan.style.display = 'block';
-    errorSpan.textContent = 'Email address must be lowercase'; 
+function showError() {
+  errorSpan.style.display = 'block';
+  errorSpan.textContent = 'Email address must be lowercase';
 }
-function hideError(){
-    errorSpan.style.display = 'none';
-    errorSpan.textContent = ''; 
+function hideError() {
+  errorSpan.style.display = 'none';
+  errorSpan.textContent = '';
 }
 
-email.addEventListener('input', () =>{
-    if(!validateEmail()){
-        showError();
-    }
-    else{
-        hideError();
-    }
-}); 
+email.addEventListener('input', () => {
+  if (!validateEmail()) {
+    showError();
+  } else {
+    hideError();
+  }
+});
 
-/* if the validation is invalid stop form from send */ 
-form.addEventListener('submit', function(event){
-    if(!validateEmail()){ 
-        showError();
-        event.preventDefault();
-    }
+/* if the validation is invalid stop form from send */
+form.addEventListener('submit', (event) => {
+  if (!validateEmail()) {
+    showError();
+    event.preventDefault();
+  }
 });
