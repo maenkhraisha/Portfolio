7f1abc744fe581d655db77119dfab04c09c1a57f
maen <maen.1980@yahoo.com>
2022-03-05 13:07:49 +0300

    fix the form not submiting

--- validation.js
+++ validation.js
@@ -1,39 +1,36 @@
-// let errorSpan = document.getElementById('error');
-// let email = document.getElementById('email');
-// let form = document.getElementsByTagName('form');
+let errorSpan = document.getElementById('error');
+let email = document.getElementById('email');
+let form = document.getElementById('contact-form');
 
 
-// function validateEmail(){
-//     const lowerCase = /^[a-z0-9.@]+$/;
-//     let result = lowerCase.test(email.value);
-//     return result;
-// }
+function validateEmail(){
+    const lowerCase = /^[a-z0-9.@]+$/;
+    let result = lowerCase.test(email.value);
+    return result;
+}
 
-// function showError(){
-//     errorSpan.style.display = 'block';
-//     errorSpan.textContent = 'Email address must be lowercase'; 
-// }
-// function hideError(){
-//     errorSpan.style.display = 'none';
-//     errorSpan.textContent = ''; 
-// }
+function showError(){
+    errorSpan.style.display = 'block';
+    errorSpan.textContent = 'Email address must be lowercase'; 
+}
+function hideError(){
+    errorSpan.style.display = 'none';
+    errorSpan.textContent = ''; 
+}
 
-// email.addEventListener('input', () =>{
-//     if(!validateEmail()){
-//         showError();
-//     }
-//     else{
-//         hideError();
-//     }
-// }); 
+email.addEventListener('input', () =>{
+    if(!validateEmail()){
+        showError();
+    }
+    else{
+        hideError();
+    }
+}); 
 
-//     /* if the validation is invalid stop form from send */ 
-// form.addEventListener('submit', function(event){
-//     if(!validateEmail()){ 
-//         showError();
-//         event.preventDefault();
-//     }
-//     else{
-//         form.submit();
-//     }
-// });
+/* if the validation is invalid stop form from send */ 
+form.addEventListener('submit', function(event){
+    if(!validateEmail()){ 
+        showError();
+        event.preventDefault();
+    }
+});
